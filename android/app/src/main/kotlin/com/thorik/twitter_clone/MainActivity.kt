038de9db9daaf6a21b1f7ebbdaf2e9d92f486f30
Thorik123 <tronikthorik07@gmail.com>
2023-06-11 18:39:44 +0700

    Setup Project

--- android/app/src/main/kotlin/com/thorik/twitter_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/thorik/twitter_clone/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.twitter_clone
+package com.thorik.twitter_clone
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '643d60940fd834d383e4';
+  static const String projectId = '6429331ed0ea6598e82a';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
