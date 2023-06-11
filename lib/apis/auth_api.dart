4f17b22fb395a652a9325cfab4c69a6087cdde30
Thorik123 <tronikthorik07@gmail.com>
2023-06-11 19:57:53 +0700

    Login User With Appwrite Auth

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -40,4 +40,24 @@ class AuthAPI implements IAuthAPI {
       );
     }
   }
+
+  @override
+  FutureEither<model.Session> login(
+      {required String email, required String password}) async {
+    try {
+      final session = await _account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      return right(session);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occured', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
 }
