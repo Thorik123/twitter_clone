9362ccb010cbd2afaa92602190f5a50cd86cc611
Thorik123 <tronikthorik07@gmail.com>
2023-07-15 14:24:22 +0700

    Create Tweet Screen UI Contd.

--- lib/features/tweet/view/create_tweet_view.dart
+++ lib/features/tweet/view/create_tweet_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
@@ -15,14 +17,20 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
-
+  final tweetTextController = TextEditingController();
   @override
+  void dispose() {
+    super.dispose();
+    tweetTextController.dispose();
+  }
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value; 
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
           icon: const Icon(Icons.close, size: 30),
         ),
         actions: [
@@ -44,11 +52,69 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                 children: [
                   CircleAvatar(
                     backgroundImage: NetworkImage(currentUser.profilePic),
+                    radius: 30,
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: TextField(
+                      controller: tweetTextController,
+                      style: const TextStyle(
+                        fontSize: 22,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: "What's happening?",
+                        hintStyle: TextStyle(
+                          color: Pallete.greyColor,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                      maxLines: null,
+                    ),
                   ),
                 ],
               ),
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Pallete.greyColor,
+              width: 0.3,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.galleryIcon),
+            ),
+      
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.gifIcon),
+            ),
+      
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.emojiIcon),
+            ),
+          ],
         ),
       ),
     );
